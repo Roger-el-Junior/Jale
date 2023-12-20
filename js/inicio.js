ede2cbea070befb0ed4e55a080fa6a31abdcac31
Roger-el-Junior <rogeralemanap@gmail.com>
2023-12-20 13:27:06 -0600

    ya se asen los calculos

--- js/inicio.js
+++ js/inicio.js
@@ -12,8 +12,37 @@ function iniciar(){
   /*
   let jugos, dinero, dejados, producto, perdida 
   */
-  console.log(jugos)
-  console.log(dinero)
-  console.log(dejados)
-  console.log(producto)
+  perdidaTotal()
+}
+function perdidaTotal(){
+  let total = (dinero+producto) // pesos 
+  
+  let totalVendidos = jugos-dejados
+  
+  let dejadosEnPesos = dejados*12
+  
+  let efectivoEsperado = total+dejadosEnPesos 
+  
+  jugos*=12
+  
+  console.log(efectivoEsperado+"" + jugos)
+  
+  if(efectivoEsperado < jugos){
+    console.log("falta")
+    
+    let asig = document.getElementById('perdida');
+  
+    let faltante = efectivoEsperado-jugos;
+    
+    asig.innerHTML = `<p>${faltante}</p>`;
+    
+  }else if(efectivoEsperado > jugos){
+    console.log("sobra")
+    console.log(efectivoEsperado-jugos)
+  }else if(efectivoEsperado == jugos){
+    
+    console.log("Todo esta correcto")
+  }
+
+  
 }
--- js/int1.js
+++ js/int1.js
@@ -1,111 +1,71 @@
-function pr1() {
-  let cJ = document.querySelector('input[name="cantidadJugos"]').value
-
-  let efectivo = document.querySelector('input[name="efectivo"]').value
-
-  let fiados = document.querySelector('input[name="fiados"]').value
-
-  let gasto = document.querySelector('input[name="gasto"]').value
-
-  let perdida = document.querySelector('input[name="perdida"]').value
-
-  //vemos cuanto Devereaux qedar
-  let dineroEfectivo = parseInt(efectivo)+parseInt(gasto) //ps
-
-  //Sacamos los jugos q se vendieron
-  let totalEfectivo = cJ - fiados //jugos
-
-  //vemos el efectibo total q Deveria aver
-  let total = totalEfectivo*12
-
-  //restarts para ver lo q falta del dimero
-  let perdidaTotal = total-dineroEfectivo
-
-  //checks el total dinero
-  console.log(total)
-
-  console.log(dineroEfectivo)
-
-  console.log(perdidaTotal)
-
-  //parrafo(td, total, perdida)
-  let tr = document.getElementById('martes')
-  let td = document.querySelectorAll('td')
-  console.log(td)
-
-}
-
 /*
   let efectivo = document.getElementById('efectivo')
   let fiados = document.getElementById('fiados')
   let gasto = document.getElementById('gasto')
   let perdida = document.getElementById('perdida')
-  
 */
 
-let jugos, dinero, dejados, producto, perdida 
+let jugos, dinero, dejados, producto, perdida
 
 function pasar() {
   let cj = document.getElementById('cantidadJugos')
-  
+
   let cJ = document.querySelector('input[name="cantidadJugos"]')
+  
+  parse(cj, cJ)
+  
+  jugos = parseInt(cJ.value)
+  
 
-  cj.removeChild(cJ)
-    
-  let p = document.createElement('p')
-  p.innerHTML = cJ.value
-  jugos = cJ.value
-
-
-  cj.appendChild(p)
-  //cJ.style.display = 'none'
 
 }
 
 function pasar2() {
   let efectivo = document.getElementById('efectivo')
-  
+
   let efectivo1 = document.querySelector('input[name="efectivo"]')
-
-  efectivo.removeChild(efectivo1)
-    
-  let p = document.createElement('p')
-  p.innerHTML = efectivo1.value
-  dinero = efectivo1.value
-
-  efectivo.appendChild(p)
-  //cJ.style.display = 'none'
+  
+  parse(efectivo, efectivo1)
+  
+  dinero = parseInt(efectivo1.value)
+  
+  
 
 }
 
 function pasar3() {
   let fiados = document.getElementById('fiados')
-  
+
   let fiados1 = document.querySelector('input[name="fiados"]')
+  
+  parse(fiados, fiados1)
 
-  fiados.removeChild(fiados1)
-    
-  let p = document.createElement('p')
-  p.innerHTML = fiados1.value
-  dejados = fiados1.value
-
-  fiados.appendChild(p)
-  //cJ.style.display = 'none'
+  dejados = parseInt(fiados1.value)
+  
 
 }
 
 function pasar4() {
   let gasto = document.getElementById('gasto')
-  
+
   let gasto1 = document.querySelector('input[name="gasto"]')
+  parse(gasto, gasto1)
+  
+  producto = parseInt(gasto1.value)
+  
+}
 
-  gasto.removeChild(gasto1)
-    
+function parse(vC, vI) {
+
+  if (vI.value == "") {
+    vI.value = 0
+  }
+
+  vC.removeChild(vI)
+
   let p = document.createElement('p')
-  p.innerHTML = gasto1.value
-  producto = gasto1.value
+  p.innerHTML = vI.value
 
-  gasto.appendChild(p)
-  //cJ.style.display = 'none'
+  vC.appendChild(p)
 
 }
